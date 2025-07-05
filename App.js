a9926b9071d647c7a31debf8586ec1c3952e2040
Paul Cloud <clouddropdesigns@gmail.com>
2025-07-05 00:19:02 +0000

    creating dependencies and app structure

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {GoogleSignin, statusCodes} from '@react-native-google-signin/google-signin';
+import {signInWithCredential, GoogleAuthProvider} from 'firebase/auth';
+import React from 'react';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Welcome</Text>
     </View>
   );
 }
